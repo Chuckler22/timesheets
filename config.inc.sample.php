aac8bb1f5790440d1f4d952b8a9111f3ffab5b7a
Dean Winter <chuckler@gmail.com>
2021-12-21 15:34:41 +1000

    Updating for sand box

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -4,12 +4,6 @@
     $password = "secret";
     $dbname = "timesheet";
 
-// Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) { 
-  die("Connection failed: " . mysqli_connect_error());
-}
 
 // Set Timzone
 date_default_timezone_set('Australia/Brisbane');
